175583c4fb5cfcc9de5615972db44d9cded05fd3
dagargo <dagargo@gmail.com>
2017-05-06 10:25:49 +0200

    Change excluded packages

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='Phatty',
-    version='1.2.1',
+    version='1.2.2',
     description='Library editor for Moog Little Phatty',
     author='David García Goñi',
     author_email='dagargo@gmail.com',
     url='https://github.com/dagargo/phatty',
-    packages=find_packages(exclude=['doc', 'test']),
+    packages=find_packages(exclude=['tests']),
     package_data={'phatty': ['resources/*']},
     license='GNU General Public License v3 (GPLv3)',
     install_requires=['mido>=1.2.5', 'python-rtmidi>=1.0.0'],
