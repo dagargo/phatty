4c1ec5005c7eefbbba5b9033230637def03da281
dagargo <dagargo@gmail.com>
2019-07-21 18:17:26 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='Phatty',
-    version='1.3.2',
+    version='1.3.3',
     description='Library editor for Moog Little Phatty',
     author='David García Goñi',
     author_email='dagargo@gmail.com',
